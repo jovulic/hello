02942961803d120454317798a97ee6524e0bb702
Josip Vulic <jovulic@gmail.com>
2025-03-18 20:40:36 -0400

    add(elm): add elm project

---
+++ elm/flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A hello world with Elm.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [
+            pkgs.elmPackages.elm
+          ];
+        };
+        defaultPackage = pkgs.stdenv.mkDerivation {
+          name = "hello-elm";
+          src = ./.;
+          outputs = [ "out" ];
+          buildInputs = with pkgs; [
+            elmPackages.elm
+          ];
+          buildPhase = ''
+            set -x
+            export HOME=$TMP
+            elm make src/Main.elm --output dist/index.html
+            set +x
+          '';
+          installPhase = ''
+            set -x
+            cp -r dist $out
+            set +x
+          '';
+          __noChroot = true;
+        };
+      }
+    );
+}
